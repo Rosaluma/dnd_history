f917cf2215c99522b3a98b49fd3baf0646042336
Rosaluma <miannamei@gmail.com>
2024-11-02 19:52:32 -0400

    Update script.js Fixed dates for Coneenci + Soup

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ function drawChart() {
   dataTable.addColumn({ type: "date", id: "End" });
 
   const events = [
-    ["World Events", "Event 1", new Date(13033, 2, 4), new Date(13042, 2, 4)],
-    ["Coneenci", "Birth", new Date(13044, 2, 4, 5), new Date(13044, 2, 4, 6)],
-    ["Coneenci", "Jepudrice", new Date(13045, 1, 14), new Date(13050, 2, 1)],
-    ["Coneenci", "Turuner Village", new Date(13050, 2, 4), new Date(13063, 3, 13)],
-    ["Coneenci", "Omur Town", new Date(13063, 4, 13), new Date(13065, 10, 18)],
-    ["Soup", "Origins", new Date(13049, 2, 4), new Date(13049, 2, 4)],
+    ["World Events", "Event 1", new Date(13033, 1, 4), new Date(13042, 1, 4)],
+    ["Coneenci", "Birth", new Date(13044, 1, 4, 5), new Date(13044, 1, 4, 6)],
+    ["Coneenci", "Jepudrice", new Date(13045, 0, 14), new Date(13050, 1, 1)],
+    ["Coneenci", "Turuner Village", new Date(13050, 1, 4), new Date(13063, 2, 13)],
+    ["Coneenci", "Omur Town", new Date(13063, 3, 13), new Date(13065, 9, 18)],
+    ["Soup", "Origins", new Date(13049, 1, 4), new Date(13049, 1, 4)],
     ["Filler Character", "Event 1", new Date(13046, 2, 4), new Date(13049, 2, 4)],
     ["Filler Character", "Event 2", new Date(13051, 2, 4), new Date(13054, 2, 4)],
     ["Filler Character 2", "Event 1", new Date(13056, 2, 4), new Date(13059, 2, 4)],
