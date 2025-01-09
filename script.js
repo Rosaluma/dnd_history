7c008d5b260491203882d2ed9962b21141510864
Rosaluma <miannamei@gmail.com>
2025-01-09 00:01:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
     ["Coneenci", "Aimless Adventures", new Date(13065, 9, 19), new Date(13067, 0)],
     ["Coneenci", "Campaigns with Comrades ", new Date(13067, 0), new Date(13067, 10)],
     ["Soup", "Origins", new Date(13049, 1, 4), new Date(13049, 1, 4)],
-    ["Isra Shadowfang", "Birth Of Isra", new Date(13044, 11, 20), new Date(13044, 11, 21)],
+    ["Isra Shadowfang", "Birth Of Isra", new Date(13044, 11, 21, 5), new Date(13044, 11, 21, 6)],
     ["Filler Character", "Event 2", new Date(13051, 2, 4), new Date(13054, 2, 4)],
     ["Filler Character 2", "Event 1", new Date(13056, 2, 4), new Date(13059, 2, 4)],
     ["Filler Character 2", "Event 2", new Date(13061, 2, 4), new Date(13064, 2, 4)]
